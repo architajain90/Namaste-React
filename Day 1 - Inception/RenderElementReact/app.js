70fa32bd7804cc36f75d65aebdf1234a87ff452e
architajain90 <architajain90@gmail.com>
2022-12-25 09:53:46 +0530

    Inception - JS , HTML, REACT

---
+++ Day 1 - Inception/RenderElementReact/app.js
@@ -0,0 +1,11 @@
+const heading1 = React.createElement("h1", {
+    style: { color: "red" },
+    id:"heading-one"
+  },"This is heading one");
+
+
+const heading2 = React.createElement('h2', {style: {color: "blue"}}, 'This is heading two');
+const container = React.createElement("div",{id:"container"}, [heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
