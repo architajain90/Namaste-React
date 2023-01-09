4221b4b085d7fa78cf60273d24e9296e2a38c992
architajain90 <architajain90@gmail.com>
2023-01-09 13:08:04 +0530

    Foodify - Restraunt List with static data

--- Food Delivery App - Foodify/src/components/Body.js
+++ Food Delivery App - Foodify/src/components/Body.js
@@ -1,7 +1,37 @@
+const RESTAURANT_LIST = {
+  name: "Burger king",
+  cuisines: ["Burger", "American"],
+  rating: "4.2",
+  image:
+    "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="restaurant-card">
+      <img src={RESTAURANT_LIST.image}></img>
+      <h2>{RESTAURANT_LIST.name}</h2>
+      <h3>{RESTAURANT_LIST.cuisines.join(", ")}</h3>
+      <h4>{RESTAURANT_LIST.rating} stars</h4>
+    </div>
+  );
+};
+
 const Body = () => {
   return (
     <div className="restaurant-list">
-      <h1>Body- Restaurant List</h1>
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
     </div>
   );
 };
